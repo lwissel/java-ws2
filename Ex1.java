c9dfed952addc9fa2d27c86af2888d9c3e0782ae
lwissel <lennart.wissel@gmx.net>
2014-10-08 21:20:34 +0100

    initial description

---
+++ Ex1.java
@@ -0,0 +1,26 @@
+/** Exercise 1 of WS2
+ *
+ * Program to compute whether the results in the software workshop module are
+ * sufficient to pass.
+ *
+ * Parameters are (counting x%)
+ *  - continousAssessment1 = ca1  (0.125)
+ *  - continousAssessment2 = ca2  (0.075)
+ *  - exam                 = ex   (0.7)
+ *  - teamWork             = tw   (0.1)
+ *
+ * MSc students must reach a mark of at least 50 overall
+ * ICY students must reach a mark of at least 40 overall
+ *
+ * 
+ * @author  Lennart Wissel
+ * @version 08.10.2014
+ *
+ */
+
+public class Ex1 {
+  public static int main(String[] args) {
+
+  }
+
+}
