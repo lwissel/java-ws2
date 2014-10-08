6c4980a869a726ba3f2a1d5e9cce44649f0518d4
lwissel <lennart.wissel@gmx.net>
2014-10-08 22:07:29 +0100

    add boolean passedTest(String sc...) function

--- Ex1.java
+++ Ex1.java
@@ -3,7 +3,7 @@
  * Program to compute whether the results in the software workshop module are
  * sufficient to pass.
  *
- * Parameters are (counting x%)
+ * Parameters are (weighting x%)
  *  - continousAssessment1 = ca1  (0.125)
  *  - continousAssessment2 = ca2  (0.075)
  *  - exam                 = ex   (0.7)
@@ -11,7 +11,9 @@
  *
  * MSc students must reach a mark of at least 50 overall
  * ICY students must reach a mark of at least 40 overall
+ *  - triggered via studentCategory = sc
  *
+ * Each of the parameters are expected to be in a range of 0-100
  * 
  * @author  Lennart Wissel
  * @version 08.10.2014
@@ -19,6 +21,22 @@
  */
 
 public class Ex1 {
+
+  // Tests whether someone passes the test
+  public static boolean passedTest(String sc, double ca1, double ca2, double ex, double tw){
+    double mark = 0.125*ca1 + 0.075*ca2 + 0.7*ex + 0.1*tw; // mark <= 100
+    if (sc.equalsIgnoreCase("ICY")){
+      return (mark >= 40);
+    }
+    else if (sc.equalsIgnoreCase("MSc")) {
+      return (mark >= 50);
+    }
+    else {
+      return false; // you will never pass the class when you are not MSc/ICY
+    }
+  }
+    
+  }
   public static int main(String[] args) {
 
   }
