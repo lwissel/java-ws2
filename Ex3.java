e2a4629a6f05e751ce96541c46045290895973d8
lwissel <lennart.wissel@gmx.net>
2014-10-20 15:01:25 +0100

    seems to work, check logic of if condition in main loop

--- Ex3.java
+++ Ex3.java
@@ -17,12 +17,51 @@
 public class Ex3 {
   public static void main(String[] args){
     // use the first argument to define the limit n
+    int n = 0; // avoid a null array
     if (args.length == 0) {
       System.out.println("Please call the program with a parameter of int n.");
       System.out.println("Usage: java program n.");
       System.exit(0);
     } else {
-      int n = Integer.parseInt(args[0]);
+      n = Integer.parseInt(args[0]);
     }
+
+    // prime array representing all the numbers from 0 up to n
+    // after the algorithm: true = prime, false  = not prime
+    // default value is FALSE for every element
+    boolean[] prime = new boolean[n+1]; // we start with 0;
+
+    // algorithm
+    for (int i = 2; i < prime.length; i++) { prime[i] = true; } // set whole array to true except for 0 and 1
+
+    // main loop to check for multiples of each next element
+    for (int q = 2; q*q <= n; q++) {
+      // but only calculate if the number is not already multiple = set to false
+      if (prime[q] == true) {
+        findPrime(q,prime);
+        System.out.println();
+        for (boolean a: prime) { System.out.print(a + " "); }
+      }
+    }
+
+    // Print the prime numbers
+    System.out.println();
+    for (int b = 0; b < prime.length; b++) {
+      if (prime[b]) {
+        System.out.print(b + " ");
+      }
+    }
+  }
+
+  // find multiples of number in boolean array and set element to false if the
+  // corresponding index is multiple because then the number can't be prime
+  // we start from m+1 wich is the next element.
+  public static boolean[] findPrime(int m, boolean[] array) {
+    for (int i = m+1; i < array.length; i++) {
+      if (i % m == 0) {
+        array[i] = false;
+      }
+    }
+    return array;
   }
 }
