d66f790b4d68fcd96d8b200124c02f723aa933f3
lwissel <lennart.wissel@gmx.net>
2014-10-22 20:48:11 +0100

    add empty method structure

---
+++ Ex5.java
@@ -0,0 +1,36 @@
+/** Exercise 5 ws 2
+ *
+ * Program to check a given sudoku solution whether it is valid
+ *
+ * the numbers 1-9 are only allowed to appear exactly once in each row, column
+ * and 3x3 subblock matrix of the 9x9 main sudoku matrix
+ *
+ * @author  Lennart Wissel
+ * @version 22.10.2014
+ */
+public class Ex5 {
+  public static void main(String[] args) { 
+
+  }
+
+  // method to check the rows to have each number only once
+  // @return true if each only once
+  public static boolean checkRow(int[][] matrix) {
+  }
+
+  // method to check the columns to have each number only once
+  // @return true if each only once
+  public static boolean checkColumn(int[][] matrix) {
+  }
+
+  // method to check the subblock matrices for the occurence of each number
+  // @return true if each only once
+  public static boolean checkSquare(int[][] matrix) {
+  }
+
+  // method to obtain a 3x9 result array containing whether each row or
+  // columns evaluates to true/false according to the sudoku excercise. The
+  // last row contains the same information for the subsquares.
+  public static boolean[][] checkSudoku(int[][] matrix) {
+  }
+}
