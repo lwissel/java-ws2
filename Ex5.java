6b09870ea85468eeca61afa3664a3a4f82e1750a
lwissel <lennart.wissel@gmx.net>
2014-10-22 23:07:47 +0100

    add final Ex5 class

--- Ex5.java
+++ Ex5.java
@@ -14,12 +14,11 @@
  */
 public class Ex5 {
   public static void main(String[] args) { 
-    int[][] input = { { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+    int[][] input2 = { { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
             { 4, 5, 6, 7, 8, 9, 1, 2, 3 }, { 7, 8, 9, 1, 2, 3, 4, 5, 6 },
             { 2, 3, 4, 5, 6, 7, 8, 9, 1 }, { 5, 6, 7, 8, 9, 1, 2, 3, 4 },
             { 8, 9, 1, 2, 3, 4, 5, 6, 7 }, { 3, 4, 5, 6, 7, 8, 9, 1, 2 },
             { 6, 7, 8, 9, 1, 2, 3, 4, 5 }, { 9, 1, 2, 3, 4, 5, 6, 7, 8 } };
-
 
     //run and print the resulting table for convienience
     boolean[][] res = checkSudoku(input);
